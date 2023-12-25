8ba8b9640899637b8b165a50101e386bdf6f0e5c
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2023-12-25 03:36:13 +0330

    Create and show a sdl window

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 
 #include <SDL2/SDL.h>
 
+namespace {
+
+constexpr auto kWinX{ 0 };
+constexpr auto kWinY{ 0 };
+constexpr auto kWinHeight{ 480 };
+constexpr auto kWinWidth{ 640 };
+constexpr auto kWinTitle{ "OpenGL Window" };
+
+} // namespace
+
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -9,6 +19,26 @@ int main(int argc, char* argv[])
     } else {
         std::cout << "SDL video system is ready to go\n";
     }
+
+    SDL_Window* window = SDL_CreateWindow(kWinTitle, kWinX, kWinY, kWinWidth, kWinHeight, SDL_WINDOW_SHOWN);
+
+#ifdef __APPLE__
+    bool quitApp = false;
+    while (!quitApp) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quitApp = true;
+            }
+        }
+    }
+#else
+    // Does not work on macos
+    SDL_Delay(3000);
+#endif
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
